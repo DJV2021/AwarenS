e8eda893d2e3aea2512e133cfb7c84b96a91484b
Sillicat <91664121+Sillicat@users.noreply.github.com>
2022-02-18 10:48:43 +0100

    Revert "Simulation de foule"
    This reverts commit c24cf64621d81c391315c2b8f2fe57f73b98dd53.

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class AgentController : MonoBehaviour
 {
     public Vector2 generalDirection = new Vector2(1f,0f);
-    public bool isDestination = false;
     public float speed = 1f;
     public float FOVRadius = 5f;
 
@@ -14,25 +13,9 @@ public class AgentController : MonoBehaviour
     public float coefDistanciation = 1f;
     public float coefGroupement = 1f;
 
-    public float varSpeed = 0f;
-    public float varFOV = 0f;
-    public float varInertie = 0f;
-    public float varDirection = 0f;
-    public float varDistanciation = 0f;
-    public float varGroupement = 0f;
-
 
 
     void Awake() {
-        varSpeed /= 100;
-        varFOV /= 100;
-        varInertie /= 100;
-        varDirection /= 100;
-        varDistanciation /= 100;
-        varGroupement /= 100;
-
-        if (!isDestination) {
-            generalDirection.Normalize();
-        }
+        generalDirection.Normalize();
     }
 }
