37a1bad3c4e0b502c21ddaabb8d383a8d635d281
Clémence <clemence.clavel@hotmail.fr>
2022-03-11 11:59:38 +0100

    modeles gris et outline

---
+++ Assets/Scripts/Modele.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modele : MonoBehaviour
+{
+    [SerializeField]
+    public Outline hair;
+    [SerializeField]
+    public Outline pants;
+    [SerializeField]
+    public Outline sleeves;
+    public bool IsModeleAWoman;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hair.eraseRenderer = true;
+        pants.eraseRenderer = true;
+        sleeves.eraseRenderer = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetModeleRed()
+    {
+        hair.eraseRenderer = false;
+        hair.color = 0;
+        pants.eraseRenderer = false;
+        pants.color = 0;
+        sleeves.eraseRenderer = false;
+        sleeves.color = 0;
+    }
+
+    public void SetModeleGreen()
+    {
+        hair.eraseRenderer = false;
+        hair.color = 1;
+        pants.eraseRenderer = false;
+        pants.color = 1;
+        sleeves.eraseRenderer = false;
+        sleeves.color = 1;
+    }
+
+    public void StopOutlineModele()
+    {
+        hair.eraseRenderer = true;
+        pants.eraseRenderer = true;
+        sleeves.eraseRenderer = true;
+    }
+}
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 // Classe gérant l'attribution de modèle et l'orientation du prefab Person.
@@ -13,35 +14,44 @@ using UnityEngine;
 
 
 public class Person : MonoBehaviour {
-    public GameObject model1;
-    public GameObject model2;
-    public GameObject model3;
-    public GameObject model4;
-    public GameObject model5;
-    public GameObject model6;
-    public GameObject model7;
-    public GameObject model8;
+    public Modele model1;
+    public Modele model2;
+    public Modele model3;
+    public Modele model4;
+    public Modele model5;
+    public Modele model6;
+    public Modele model7;
+    public Modele model8;
 
     private int personModel;        // le modèle pris par le GameObject
     private Rigidbody personBody;   // le RigidBody du Gameobject
     private float personAngle;      // l'angle que fait le GameObject avec Vector3.forward
 
+    public bool isAffected;
+    private Modele model;
+    public bool isWoman;
+    public Button yourButton;
 
     // Choisit un modèle au hasard et l'applique au GameObject
     private void Start() {
         personModel = Random.Range(1, 9);
         personBody = GetComponent<Rigidbody>();
-
-        switch (personModel) {
-            case 1: Instantiate(model1, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 2: Instantiate(model2, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 3: Instantiate(model3, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 4: Instantiate(model4, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 5: Instantiate(model5, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 6: Instantiate(model6, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 7: Instantiate(model7, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
-            case 8: Instantiate(model8, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+        isAffected = false;
+        switch (personModel)
+        {
+            case 1: model = Instantiate(model1, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 2: model = Instantiate(model2, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 3: model = Instantiate(model3, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 4: model = Instantiate(model4, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 5: model = Instantiate(model5, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 6: model = Instantiate(model6, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 7: model = Instantiate(model7, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
+            case 8: model = Instantiate(model8, transform.position, Quaternion.Euler(Vector3.zero), transform); break;
         }
+
+        isWoman = model.IsModeleAWoman;
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
     }
 
 
@@ -49,5 +59,25 @@ public class Person : MonoBehaviour {
     private void Update() {
         personAngle = Vector3.SignedAngle(Vector3.forward, personBody.velocity, Vector3.up);
         transform.rotation = Quaternion.Euler(personAngle * Vector3.up);
+    }
+
+    public void SetGreen()
+    {
+        model.SetModeleGreen();
+    }
+
+    public void SetRed()
+    {
+        model.SetModeleRed();
+    }
+
+    public void StopOutline()
+    {
+        model.StopOutlineModele();
+    }
+
+    void TaskOnClick()
+    {
+        SetGreen();
     }
 }
